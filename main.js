0c941a1b58237c4d0fbd50065d2525f19767b9f3
KRISH-SINGLA <85232141+KRISH-SINGLA@users.noreply.github.com>
2022-04-22 17:59:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var name_of_the_student_array= [];
+function submit(){
+    var display_student_array= [];
+    for(var j= 1;j<=4;j++){
+        var name_of_the_student= 
+        document.getElementById("name_of_the_student"+j).value;
+        name_of_the_student_array.push("name_of_the_student");
+    }
+var length_of_name= name_of_the_student_array.length;
+for(var k=0;k<length_of_name;k++){
+    display_student_array.push("<h4>name"+name_of_the_student_array[k]+"</h4>");
+    document.getElementById("display_name_with_commas").innerHTML= display_student_array;
+}
+}
